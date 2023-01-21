96459319acf0b62f907b60a0eb11421128aab2c1
hoochiecoo <waveinspectro@gmail.com>
2023-01-21 17:51:28 +0300

    add prints

--- igra1.py
+++ igra1.py
@@ -1,5 +1,7 @@
 previous_values = []
+steps = []
 multiply_by_2 = False
+sum = 0
 
 while True:
     try:
@@ -10,6 +12,10 @@ while True:
             value = value * 2
             multiply_by_2 = False
         previous_values.append(value)
+        sum += value
+        steps.append({'value': value, 'sum': sum, 'previous_values': previous_values.copy()})
+        print("Current value:", value)
+        print("Current sum:", sum)
         print("Previous values:", previous_values)
     except ValueError:
         print("Invalid input. Please enter an integer.")
