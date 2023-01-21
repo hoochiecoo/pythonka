ef36e495c2b7519a051a5402e91321eb2cdee3a9
hoochiecoo <waveinspectro@gmail.com>
2023-01-21 17:23:19 +0300

    multiply added

--- igra1.py
+++ igra1.py
@@ -1,7 +1,13 @@
 previous_values = []
+multiply_by_2 = False
+
 while True:
     try:
         value = int(input("Please enter an integer value: "))
+        if value == 10:
+            multiply_by_2 = True
+        if multiply_by_2:
+            value = value * 2
         previous_values.append(value)
         print("Previous values:", previous_values)
     except ValueError:
